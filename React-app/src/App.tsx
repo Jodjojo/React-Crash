717301602a9450d802f2b2eb372c1ad92015ec3a
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-05-23 16:10:34 +0100

    Creating ListGroup Components using Bootstrap

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -1,11 +1,11 @@
 // First you import your Component file
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 // Now to use the imported component we use it just as a normal HTML component tag with opening and closing tag
 function App() {
 	return (
 		<div>
-			<Message></Message>
+			<ListGroup></ListGroup>
 		</div>
 	);
 }
--- React-app/src/main.tsx
+++ React-app/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// Importing Bootstrap
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Component Tree rendered for this using React Dom(not limited to mobile building like react native) or rendered for  A mobile app using React Native
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
