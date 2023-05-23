491319245aa01edfbfb1eacbd173e1174712053d
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-05-23 16:19:18 +0100

    Using Fragments with Multiple HTML elements

--- React-app/src/components/ListGroup.tsx
+++ React-app/src/components/ListGroup.tsx
@@ -1,14 +1,60 @@
 function ListGroup() {
 	// Importing ListGroup Markup from the Bootstrap Documentation
 	return (
-		<ul className="list-group">
-			<li className="list-group-item">An item</li>
-			<li className="list-group-item">A second item</li>
-			<li className="list-group-item">A third item</li>
-			<li className="list-group-item">A fourth item</li>
-			<li className="list-group-item">And a fifth one</li>
-		</ul>
+		<>
+			<h1>List</h1>
+			<ul className="list-group">
+				<li className="list-group-item">An item</li>
+				<li className="list-group-item">A second item</li>
+				<li className="list-group-item">A third item</li>
+				<li className="list-group-item">A fourth item</li>
+				<li className="list-group-item">And a fifth one</li>
+			</ul>
+		</>
 	);
 }
 
+// In react a component cannot return more than one component...so we can return this listGroup and still return a header under this component
+// So we can Either use FRAGMENTS to solve this problem or wrap everything with a div
+// or use empty tags to tell react were using fragments
+////////////////////////////FRAGMENTS/////////////////////////
+/*
+import { Fragment } from "react";
+
+function ListGroup() {
+	// Importing ListGroup Markup from the Bootstrap Documentation
+	return (
+		<Fragment>
+			<h1>List</h1>
+			<ul className="list-group">
+				<li className="list-group-item">An item</li>
+				<li className="list-group-item">A second item</li>
+				<li className="list-group-item">A third item</li>
+				<li className="list-group-item">A fourth item</li>
+				<li className="list-group-item">And a fifth one</li>
+			</ul>
+		</Fragment>
+	);
+}
+*/
+// Fragments allows us to solve this problem without creating an external DIV element in the DOM
+
+///////////////////////////////////////////////////////////////
+//////////////////////DIV//////////////////////////////////////
+/*
+function ListGroup() {
+	// Importing ListGroup Markup from the Bootstrap Documentation
+	return (
+		<div>
+			<h1>List</h1>
+			<ul className="list-group">
+				<li className="list-group-item">An item</li>
+				<li className="list-group-item">A second item</li>
+				<li className="list-group-item">A third item</li>
+				<li className="list-group-item">A fourth item</li>
+				<li className="list-group-item">And a fifth one</li>
+			</ul>
+		</div>
+	);
+*/
 export default ListGroup;
